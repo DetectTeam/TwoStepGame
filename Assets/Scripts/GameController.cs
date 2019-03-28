181b8e7c8833deef906c5214ff7e225afc4b11bd
Eoghan <eoghan.gallagher@gmail.com>
2019-03-28 16:51:02 +0000

    Update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -27,6 +27,7 @@ public class GameController : MonoBehaviour
 
 
     [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private GameObject dudBallPrefab;
 
     [SerializeField] private GameObject muzzle;
 
@@ -36,24 +37,86 @@ public class GameController : MonoBehaviour
 
     [SerializeField] private float speed;
 
+     private bool mouseDrag;
+
+     private bool isReloaded = true;
+
+    [SerializeField] private bool isWhiteBullet = true;
+    [SerializeField] private bool isDud;
+
+    [SerializeField] private Color white;
+    [SerializeField] private Color red;
+
+
     public enum Direction
     {
         LEFT,
         RIGHT
     }
 
-
-    private bool mouseDrag;
-    private void Update()
+    private void Start()
     {
-         if( mouseDrag )
-         {
-            Quaternion targetRotation = Quaternion.identity;
-            targetRotation *=  Quaternion.AngleAxis(z, Vector3.forward);
-
-            compass.transform.rotation = Quaternion.Lerp ( compass.transform.rotation, targetRotation , ( Time.time * speed ) ); 
-         }
+        compass.transform.rotation = Quaternion.identity;
     }
+
+
+    [SerializeField] private float angle;
+
+	private void Update()
+    {
+        //rotation:
+		//Vector3 mousePos = new Vector3( Input.mousePosition.x,Input.mousePosition.y,Camera.main.transform.position.y );
+		//Vector3 worldPos = Camera.main.ScreenToWorldPoint( mousePos );
+		///iTween.LookUpdate( compass ,iTween.Hash( "looktarget", worldPos, "time", 2, "axis", "x" ) );
+      
+        //Get the Screen positions of the object
+         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint ( compass.transform.position );
+         
+         //Get the Screen position of the mouse
+         Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+         
+         //Get the angle between the points
+         angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+         
+        angle+= 80;
+       
+
+         if( angle >= 80 )
+            angle = 80;
+
+         if( angle <= -80 )
+            angle = -80;  
+ 
+         //Ta Daaa
+         compass.transform.rotation =  Quaternion.Euler ( new Vector3( 0f,0f,angle ) );
+
+         if ( Input.GetMouseButtonUp(0)  && !IsOverUI() && isReloaded )
+         {
+            Fire();
+            isReloaded = false;
+         }
+
+		//fire:
+		// if(Input.GetMouseButtonDown(0)){
+		// 	Fire();
+		// }
+	}
+
+     float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+
+
+    // private void Update()
+    // {
+    //      if( mouseDrag )
+    //      {
+    //         Quaternion targetRotation = Quaternion.identity;
+    //         targetRotation *=  Quaternion.AngleAxis(z, Vector3.forward);
+
+    //         compass.transform.rotation = Quaternion.Lerp ( compass.transform.rotation, targetRotation , ( Time.time * speed ) ); 
+    //      }
+    // }
 
     [SerializeField] private Direction currentDirection;
 
@@ -67,13 +130,13 @@ public class GameController : MonoBehaviour
     {
         
 
-       CalculateClickPosition();
-       CalculateDirection();
+       //CalculateClickPosition();
+       //CalculateDirection();
 
        float dist = Vector3.Distance(initialPosition, cursorPosition);
        
-       if( dist > 0.5f )
-        RotateCompass();
+       //if( dist > 0.5f )
+        //RotateCompass();
     }
 
     private void OnMouseUp()
@@ -139,12 +202,41 @@ public class GameController : MonoBehaviour
 
     private void Fire()
     {
-        var ball = Instantiate( ballPrefab, muzzle.transform.position, muzzle.transform.rotation ) as GameObject;
-        ball.gameObject.SetActive(true);
+        //var ball = Instantiate( ballPrefab, muzzle.transform.position, muzzle.transform.rotation ) as GameObject;
+        //ball.gameObject.SetActive(true);
+
+        if ( isWhiteBullet )
+                isDud = CheckForDud( 20 );
+            else
+                isDud = CheckForDud( 80 );
+
+        GameObject ball = CreateBall( isDud  );
+
+        ball.transform.position = muzzle.transform.position;
+        ball.transform.rotation = muzzle.transform.rotation;
+
+         if( isWhiteBullet )
+            ball.GetComponent<SpriteRenderer>().color = white;
+        else
+            ball.GetComponent<SpriteRenderer>().color = red;
         
+        ball.gameObject.SetActive(true);
         
        // ball.GetComponent<Rigidbody2D>().AddForce(-direction);
         ball.GetComponent<Rigidbody2D>().AddForce( muzzle.transform.up * 100 );
+    }
+
+    public void Reload()
+    {
+        isReloaded = true;
+        Messenger.Broadcast( "DisableReloadButtons" );
+        StartCoroutine( EnableReloadButtons( ) );
+    }
+
+    private IEnumerator EnableReloadButtons()
+    {
+        yield return new WaitForSeconds( 1.0f );
+         Messenger.Broadcast( "EnableReloadButtons" );
     }
 
     public void SetSliderSensitivity( float value )
@@ -152,5 +244,75 @@ public class GameController : MonoBehaviour
         sliderSensitivity.text = value.ToString();
         numDegrees = value;
     }
+
+    public static bool IsOverUI()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+
+        pointerData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        if (results.Count > 0)
+        {
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i].gameObject.GetComponent<CanvasRenderer>())
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    } 
+
+    public bool CheckForDud( float percentage )
+    {
+        bool b;
+        var rand = Random.value;
+
+        Debug.Log( "Check For Dud: " + rand + " " + ( percentage / 100 ) );
+
+        //Weighted Random goes here
+       if( rand < ( percentage / 100 ) )
+        b = true;
+       else
+        b = false;
+
+        return b;
+    }
+
+    public void BulletColourProbability( float percentage )
+    {  
+        var rand = Random.value;
+        bool b = false;
+
+        Debug.Log( "Rand: " + rand + " " + percentage / 100 );
+        
+        if ( rand < ( percentage / 100 ) )
+            isWhiteBullet = true;
+        else 
+            isWhiteBullet = false;       
+        
+    }
+
+      public GameObject CreateBall( bool b )
+    {
+        GameObject ball = null;
+
+        if( b )
+        { 
+            ball = Instantiate( dudBallPrefab ) as GameObject;
+        }
+        else
+        {
+             ball = Instantiate( ballPrefab ) as GameObject;
+        }
+     
+        return ball;
+    }
+    
    
 }
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Switch : MonoBehaviour
+{ 
+    [SerializeField] private float delay;
+    [SerializeField] private GameObject gate;
+    [SerializeField] private TextMeshPro counter;
+    [SerializeField] private SpriteRenderer foreGround;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if( !gate )
+            Debug.Log( "Gate Not set.." );
+
+        if( !counter )
+            Debug.Log( "Counter not set..." );
+    }
+
+    private void OnTriggerEnter2D( Collider2D collider )
+    {
+      if( collider.CompareTag( "Ball" ) )
+      {
+          StartCoroutine( GateControl() );
+      }
+    }
+
+    private IEnumerator GateControl()
+    {
+        
+        float H, S, V;
+
+       Color switchColor = foreGround.color;
+        
+        gate.SetActive( false );
+        GetComponent<Collider2D>().enabled = false;
+
+        Color.RGBToHSV(  switchColor , out H, out S, out V );
+        ChangeHue( H + 0.025f );
+
+        for( int x = 0; x < delay; x++ )
+        {
+            //counter.text = ( delay - x ).ToString();
+            yield return new WaitForSeconds( 1f );
+           
+        }
+        ChangeHue( H );
+
+        counter.text = "";
+
+        gate.SetActive( true );
+         GetComponent<Collider2D>().enabled = true;
+    }
+
+    private void ChangeHue( float hueValue )
+    {
+        foreGround.color = Color.HSVToRGB(hueValue, 0.6f, 0.8f); 
+    }
+ 
+}
