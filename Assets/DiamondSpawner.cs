fba3a9cbc2f1fcb6d71e183e5cbc8957d9213ca2
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2019-04-24 17:48:04 +0100

    RandomLevel Update

---
+++ Assets/DiamondSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject diamondContainer;
+    [SerializeField] private GameObject diamondWall;
+
+    [SerializeField] private float defaultRotation;
+
+    [SerializeField] private List<float> rotations = new List<float>();
+    private void Awake()
+    {
+        if( !diamondContainer )
+        {
+            Debug.Log( "Diamond Container not set" );
+            return;
+        }
+    }
+    private void OnEnable()
+    {
+        //Activate diamond Container
+        diamondContainer.SetActive( true );
+        diamondWall.transform.localRotation = Quaternion.Euler( 0, 0, rotations[ Random.Range( 0, rotations.Count - 1 ) ] );
+
+        
+
+        //Set its rotation;
+    }
+    // Start is called before the first frame update
+
+    private void OnDisable()
+    {
+        //Reset diamond container rotation
+        diamondWall.transform.localRotation = Quaternion.Euler( 0, 0, defaultRotation );
+
+        //Disable
+        diamondContainer.SetActive( false );
+    }
+}
---
+++ Assets/RandomLevelGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace twostep
+{
+    public class RandomLevelGenerator : MonoBehaviour
+    {
+         [SerializeField] private List<GameObject> spawners = new List<GameObject>();
+        // Start is called before the first frame update
+        private IEnumerator Start()
+        {
+           while( true )
+           {
+               
+               DisableSpawners();
+               PickRandomSpawner();
+
+
+               yield return new WaitForSeconds( 3.0f );
+               
+
+                //Disable all spawners
+
+               //Pick a random spawner and activate it.
+           }
+        }
+
+        private void DisableSpawners()
+        {
+            foreach( GameObject spawner in spawners )
+                spawner.SetActive( false );
+        }
+
+
+        private int lastRand;
+        private void PickRandomSpawner()
+        {
+            int rand = Random.Range( 0 , spawners.Count -1 );
+
+            while( rand == lastRand )
+            {
+                rand = Random.Range( 0 , spawners.Count -1 );
+                Debug.Log( "Match" );
+            }
+
+            spawners[ rand ].SetActive( true );
+
+            lastRand = rand;
+        }
+
+       
+    }
+}
