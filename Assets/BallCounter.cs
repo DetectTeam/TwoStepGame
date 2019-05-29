335dfee0b4028acd43dcb296f00dddad9936143d
Eoghan <eoghan.gallagher@gmail.com>
2019-05-29 17:30:03 +0100

    Added Ball Count

---
+++ Assets/BallCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BallCounter : MonoBehaviour
+{
+    
+    [SerializeField] private TextMeshProUGUI ballCount;
+
+    private int totalBalls = 10;
+
+    private void OnEnable()
+    {
+        Messenger.AddListener( "DecrementBallCount", DecrementBallCount );
+    }
+
+    private void OnDisable()
+    {
+        Messenger.RemoveListener( "DecrementBallCount", DecrementBallCount );
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DecrementBallCount()
+    {
+        totalBalls --;
+
+        if( totalBalls > 0 )
+        {
+            ballCount.text = totalBalls.ToString();
+        }
+        else if( totalBalls <= 0 )
+        {
+            ballCount.text = "0";
+            Debug.Log( "Level Over..." );
+        }
+    }
+
+    
+}
